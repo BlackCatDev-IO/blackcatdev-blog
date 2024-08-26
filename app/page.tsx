2aa8e747c2ef5dc9aa7959b7dede1def0f3008cb
Loren Aguey <loren@blackcatdev.io>
2024-08-25 22:11:43 -0400

    Add BlogPost component

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
 import Image from 'next/image';
 import styles from './page.module.css';
+import BlogPost from './blog-post/blog-post';
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
-        <h1>Blog</h1>
+        <BlogPost />
       </div>
     </main>
   );
