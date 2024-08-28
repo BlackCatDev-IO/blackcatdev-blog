bef757d80051960383ea0746df6779f3022f16a5
Loren Aguey <loren@blackcatdev.io>
2024-08-27 23:25:58 -0400

    Implement Incremental Static Regeneration for blog posts

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,45 @@
 import Image from 'next/image';
-import styles from './page.module.css';
-import BlogPost from './blog-post/blog-post';
+import Link from 'next/link';
+import { getBlogPosts } from './api/fetch-blogs';
+import { BlogPostData } from './post/[id]/types/blog';
 
-export default function Home() {
+export default async function Home() {
+  const blogs = await getBlogPosts();
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <BlogPost />
+    <main>
+      <h1>Blog</h1>
+      <div>
+        <ul>
+          {blogs.map((blog, index) => (
+            <li key={index}>
+              <Link href={`/post/${blog.id}`} key={blog.id}>
+                <BlogCard blog={blog} />
+              </Link>
+            </li>
+          ))}
+        </ul>
       </div>
     </main>
+  );
+}
+
+function BlogCard({ blog: blog }: { blog: BlogPostData }) {
+  const imageAttributes = blog.attributes.image.data[0].attributes;
+  const baseUrl = process.env.baseUrl;
+  const imageUrl = `${baseUrl}${imageAttributes.url}`;
+
+  return (
+    <div>
+      <h2>{blog.attributes.title}</h2>
+      <p>{blog.attributes.subTitle}</p>
+      <Image
+        src={imageUrl}
+        key={imageAttributes.name}
+        alt={blog.attributes.title}
+        width={100}
+        height={100}
+      />
+    </div>
   );
 }
---
+++ app/post/[id]/page.tsx
@@ -0,0 +1,98 @@
+import { getBlogPosts, getBlogPostsById } from '../../api/fetch-blogs';
+import { ContentBlock } from './types/blog';
+import Image from 'next/image';
+
+export async function generateStaticParams() {
+  const posts = await getBlogPosts();
+
+  return posts.map((post) => ({
+    id: post.id.toString(),
+  }));
+}
+
+export default async function Page({ params }: { params: { id: string } }) {
+  const blog = await getBlogPostsById({ id: params.id });
+
+  return <RenderContent content={blog.attributes.content} />;
+}
+
+const RenderContent: React.FC<{ content: ContentBlock[] }> = ({ content }) => {
+  return content.map((block, index) => {
+    switch (block.type) {
+      case 'heading':
+        const HeadingTag = `h${block.level}` as keyof JSX.IntrinsicElements;
+        return <HeadingTag key={index}>{block.children[0].text}</HeadingTag>;
+
+      case 'paragraph':
+        return (
+          <p key={index}>
+            {block.children.map((child, idx) => {
+              switch (true) {
+                case !!child.bold:
+                  return <strong key={idx}>{child.text}</strong>;
+                case !!child.italic:
+                  return <em key={idx}>{child.text}</em>;
+                case !!child.underline:
+                  return <u key={idx}>{child.text}</u>;
+                case !!child.strikethrough:
+                  return <del key={idx}>{child.text}</del>;
+                case child.type === 'link':
+                  return (
+                    <a key={idx} href={child.url}>
+                      {child.children ? child.children[0].text : child.text}
+                    </a>
+                  );
+                default:
+                  return child.text;
+              }
+            })}
+          </p>
+        );
+
+      case 'list':
+        if (block.format === 'ordered') {
+          return (
+            <ol key={index}>
+              {block.children.map((item, idx) => (
+                <li key={idx}>{item.children?.[0]?.text ?? ''}</li>
+              ))}
+            </ol>
+          );
+        } else {
+          return (
+            <ul key={index}>
+              {block.children.map((item, idx) => (
+                <li key={idx}>{item.children?.[0]?.text}</li>
+              ))}
+            </ul>
+          );
+        }
+
+      case 'image':
+        const image = block.image;
+
+        return (
+          <Image
+            key={index}
+            src={image?.url || ''}
+            alt={image?.alternativeText || ''}
+            width={image?.width}
+            height={image?.height}
+          />
+        );
+
+      case 'quote':
+        return <blockquote key={index}>{block.children[0].text}</blockquote>;
+
+      case 'code':
+        return (
+          <pre key={index}>
+            <code>{block.children[0].text}</code>
+          </pre>
+        );
+
+      default:
+        return null;
+    }
+  });
+};
