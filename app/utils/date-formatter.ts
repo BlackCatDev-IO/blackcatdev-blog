04c67efe92e0e684c58e833e6e91347513ee2512
Loren Aguey <loren@blackcatdev.io>
2024-09-01 21:03:50 -0400

    Add date-formatter.ts

---
+++ app/utils/date-formatter.ts
@@ -0,0 +1,11 @@
+const formatDate = (dateString: string): string => {
+  const date = new Date(dateString);
+
+  return new Intl.DateTimeFormat('en-US', {
+    month: 'short',
+    day: 'numeric',
+    year: 'numeric',
+  }).format(date);
+};
+
+export default formatDate;
